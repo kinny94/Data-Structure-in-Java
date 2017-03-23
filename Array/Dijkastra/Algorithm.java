4ce978f5641fa5267f239e96e0cda04f6aff9b77
Arjun  Dass <ad.sam007@gmail.com>
2017-03-23 15:21:23 -0500

    Implemented dijkastra

---
+++ Array/Dijkastra/Algorithm.java
@@ -0,0 +1,40 @@
+package DijkastraInterpreter;
+import java.util.Stack;
+
+public class Algorithm {
+	
+	private Stack<String> operationStack;
+	private Stack<Double> valueStack;
+	
+	public Algorithm(){
+		this.operationStack = new Stack<>();
+		this.valueStack = new Stack<>();
+	}
+	
+	public void interpreterExpression(String expression){
+		String[] expressionArray = expression.split(" ");
+		for(String s: expressionArray){
+			if(s.equals("(")){
+				
+			}else if(s.equals("+")){
+				this.operationStack.push(s);
+			}else if(s.equals("*")){
+				this.operationStack.push(s);
+			}else if(s.equals(")")){
+				String operation = this.operationStack.pop();
+				if(operation.equals("+")){
+					this.valueStack.push(this.valueStack.pop() + this.valueStack.pop());
+				}else if(operation.equals("*")){
+					this.valueStack.push(this.valueStack.pop() * this.valueStack.pop());
+				}
+			}else{
+				this.valueStack.push(Double.parseDouble(s));
+			}
+		}
+	}
+	
+	public void result(){
+		System.out.println(this.valueStack.pop());
+	}
+	
+}
