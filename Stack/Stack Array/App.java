c0de4937507cf093855d4f28790c299c548a55e4
Arjun  Dass <ad.sam007@gmail.com>
2017-03-22 22:19:24 -0500

    Added app.java

---
+++ Stack/Stack Array/App.java
@@ -0,0 +1,23 @@
+package StackArray;
+
+import StackLisnkedList.Stack;
+
+public class App {
+	public static void main(String args[]){
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		stack.push(100);
+		stack.push(200);
+		stack.push(300);
+		stack.push(400);
+		
+		System.out.println(stack.peek());
+		System.out.println(stack.size());
+		
+		System.out.println(stack.pop());
+		
+		System.out.println(stack.peek());
+		System.out.println(stack.size());
+	}
+}
