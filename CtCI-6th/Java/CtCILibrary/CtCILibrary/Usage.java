ed847e7ee97d8928595c0bfcb517e75ab5ad3dbb
Arjun  Dass <ad.sam007@gmail.com>
2017-03-29 11:26:33 -0500

    added ctci folder

---
+++ CtCI-6th/Java/CtCILibrary/CtCILibrary/Usage.java
@@ -0,0 +1,10 @@
+import CtCILibrary.*;
+
+public class Usage{
+
+	public static void main(String[] args) {
+
+		int i = 12;
+		AssortedMethods.randomInt(i);
+	}
+}
---
+++ CtCI-6th/PRACTICE/Java/CtCILibrary/CtCILibrary/Usage.java
@@ -0,0 +1,11 @@
+
+public class Usage{
+
+	public static void main(String[] args) {
+
+		int i = 12;
+		System.out.println(AssortedMethods.toBaseNString(5, 2));
+		System.out.println(AssortedMethods.toBaseNString(15, 8));
+		System.out.println(AssortedMethods.toBaseNString(100000, 16));
+	}
+}
