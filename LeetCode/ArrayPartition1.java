b14844497541ae3301072a20be2109934576d710
Arjun  Dass <ad.sam007@gmail.com>
2017-09-15 21:11:11 -0500

    Implemeted Array Partition

---
+++ LeetCode/ArrayPartition1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ArrayPartition1 {
+
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+
+}
