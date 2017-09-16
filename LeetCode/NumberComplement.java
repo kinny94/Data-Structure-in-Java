1ec71fced904b3d1935a292aba496474a50ef849
Arjun  Dass <ad.sam007@gmail.com>
2017-09-15 21:53:12 -0500

    Implementeda better way

--- LeetCode/NumberComplement.java
+++ LeetCode/NumberComplement.java
@@ -3,24 +3,20 @@ import java.util.*;
 
 public class NumberComplement{
   
-    
-    public static int findComplement(int digit){
+    public int findComplement(int num) 
+    {
+        int i = 0;
+        int j = 0;
         
-      StringBuilder binary = new StringBuilder(Integer.toBinaryString(digit));
-      
-      for(int i=0; i<binary.length(); i++){
-          if(String.valueOf(binary.charAt(i)).equals("1")){
-              binary.setCharAt(i, '0');
-          }else {
-            binary.setCharAt(i, '1');
-          }
-      }
-      
-      int complement = Integer.parseInt(binary.toString(), 2);
-      
-      return complement;
+        while (i < num)
+        {
+            i += Math.pow(2, j);
+            j++;
+        }
+        
+        return i - num;
     }
-  
+
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         int digit = scan.nextInt();
